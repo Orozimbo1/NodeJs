b2285c9cca5cf32f2636507db0911c5b3bdfa1eb
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2022-12-29 19:49:12 -0300

    Módulos

---
+++ 1_Modulos/A/B/C/D/modulo2.js
@@ -0,0 +1,12 @@
+// module.exports = function(x, y) {
+//     return x * y
+// }
+
+// module.exports = 2
+
+module.exports = class Animal {
+    constructor(nome, familia) {
+        this.nome = nome
+        this.familia = familia
+    }
+}
--- 1_Modulos/app.js
+++ 1_Modulos/app.js
@@ -1,5 +1,8 @@
 // const modulo1 = require('./modulo1')
 const { nome, sobrenome, nomeCompleto, Pessoa } = require('./modulo1')
+// const multiplicacao = require(''./A/B/C/D/modulo2'')
+// const numero = require(''./A/B/C/D/modulo2'')
+const Animal = require('./A/B/C/D/modulo2')
 
 const pessoa = new Pessoa('Matheus')
 
@@ -7,3 +10,15 @@ console.log(pessoa)
 console.log(nome)
 console.log(sobrenome)
 nomeCompleto()
+
+// console.log(multiplicacao(2, 2))
+// console.log(numero)
+
+const cachorro = new Animal('Cachorro', 'Canídeos')
+console.log(cachorro)
+
+const path = require('path')
+console.log(__filename) // Caminho absoluto do arquivo atual
+console.log(__dirname) // Caminho absoluto do diretorio atual
+
+console.log(path.resolve(__dirname, '..', '..', 'arquivos', 'imagens'))
